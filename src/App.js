03110df34e6e2935895f4b03b5ab58650d8800ab
vinaym001 <mvinay.vn@gmail.com>
2023-03-22 06:56:36 +0000

    commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { isSaved: false, text: "" };
+
+  onTextInput = (event) => {
+    this.setState({ text: event.target.value });
+  };
+
+  toggleBtn=()=>{
+      this.setState(prevState=>({
+          isSaved:!prevState.isSaved
+      }))
+  }
+ 
+
+  render() {
+    const { isSaved, text } = this.state;
+    return (
+      <div>
+        <div>
+          {isSaved ? (
+            <p>{text}</p>
+          ) : (
+            <input value={text} type="text" onChange={this.onTextInput} />
+          )}
+          <button type="button" onClick={this.toggleBtn}>
+            {isSaved ? "edit" : "save"}
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
